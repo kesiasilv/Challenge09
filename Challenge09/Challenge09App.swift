e40f62838ced21e1e7b331aeee9bd52a667926e2
Késia Silva <kesiasilvab6@gmail.com>
2025-10-16 17:06:28 -0300

    Inicializando repositório

---
+++ Challenge09/Challenge09App.swift
@@ -0,0 +1,17 @@
+//
+//  Challenge09App.swift
+//  Challenge09
+//
+//  Created by Késia Silva Viana on 16/10/25.
+//
+
+import SwiftUI
+
+@main
+struct Challenge09App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
